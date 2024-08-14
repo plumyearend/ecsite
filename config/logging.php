08bc46ff0fb4488c1274ad94c8edabbda8734412
plumyearend <pochachillacake@gmail.com>
2024-08-14 16:25:37 +0900

    日付別にログファイルを出力するよう対応

--- config/logging.php
+++ config/logging.php
@@ -60,14 +60,26 @@ return [
 
         'single' => [
             'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path(
+                sprintf(
+                    'logs/%s/%s',
+                    env('APP_ENV'),
+                    app()->runningInConsole() ? 'console.log' : 'laravel.log'
+                )
+            ),
             'level' => env('LOG_LEVEL', 'debug'),
             'replace_placeholders' => true,
         ],
 
         'daily' => [
             'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path(
+                sprintf(
+                    'logs/%s/%s',
+                    env('APP_ENV'),
+                    app()->runningInConsole() ? 'console.log' : 'laravel.log'
+                )
+            ),
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => env('LOG_DAILY_DAYS', 14),
             'replace_placeholders' => true,
@@ -124,7 +136,13 @@ return [
         ],
 
         'emergency' => [
-            'path' => storage_path('logs/laravel.log'),
+            'path' => storage_path(
+                sprintf(
+                    'logs/%s/%s',
+                    env('APP_ENV'),
+                    app()->runningInConsole() ? 'console.log' : 'laravel.log'
+                )
+            ),
         ],
 
     ],
