9468bbb74947da076a78a56946bd92e155485907
plumyearend <pochachillacake@gmail.com>
2024-08-18 16:35:38 +0900

    ログイン画面のviewを用意

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Authenticate::redirectUsing(function () {
+            if (Route::is('admin.*')) {
+                return route('admin.login');
+            } else {
+                return route('account.login');
+            }
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth:web')->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+});
+
+Route::prefix('account')->name('account.')->group(function () {
+    Route::get('/login', [LoginController::class, 'login'])
+        ->name('login');
 });
