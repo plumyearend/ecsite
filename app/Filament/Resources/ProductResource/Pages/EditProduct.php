92bc245043dad2db3d7662d5f273d8a21ccb0dab
plumyearend <pochachillacake@gmail.com>
2025-02-16 17:30:16 +0900

    確認モーダルを実装

--- app/Filament/Resources/ProductResource/Pages/EditProduct.php
+++ app/Filament/Resources/ProductResource/Pages/EditProduct.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\ProductResource\Pages;
 
 use App\Filament\Resources\ProductResource;
 use Filament\Actions;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\EditRecord;
 
 class EditProduct extends EditRecord
@@ -15,5 +16,17 @@ class EditProduct extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function getSaveFormAction(): Action
+    {
+        return Action::make('save')
+            ->label(__('filament-panels::resources/pages/edit-record.form.actions.save.label'))
+            // ->submit('save')
+            ->requiresConfirmation()
+            ->action(fn() => $this->save())
+            ->modalDescription(fn() => '在庫が0の状態です。保存しますか？')
+            ->modalHidden(fn() => $this->data['count'] > 0)
+            ->keyBindings(['mod+s']);
     }
 }
